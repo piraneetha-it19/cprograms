82bfced98498833c302013c9316e2af256dc0d64
piraneetha-it19 <54268012+piraneetha-it19@users.noreply.github.com>
2019-09-20 15:36:58 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+int i,a,c,n=10;
+printf("enter a number");
+scanf("%d",&a);
+for(i=1;i<=n;i++)
+{
+c=i*a;
+printf("\n %d*%d=%d",i,a,c);
+}
+}
+
---
+++ pass.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,total;
+printf("enter 5 subject marks");
+scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+total=a+b+c+d+e;
+printf("%d",total);
+if(a>50&&b>50&&c>50&&d>50&&e>50)
+{
+printf("pass");
+}
+else
+{
+printf("fail");
+}
+}
