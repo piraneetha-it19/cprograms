a8366839c5d43913c24a5fe0c978f95d50728774
piraneetha-it19 <54268012+piraneetha-it19@users.noreply.github.com>
2019-10-10 12:18:05 +0530

    third commit

---
+++ descending.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+   
+    int a[5],i,j,temp;
+
+    printf("enter numbers");
+
+    for(int i=0;i<5;i++)
+
+    {
+        
+     scanf("%d",&a[i]);
+
+    }
+
+      for(i=0;i<5;i++)
+
+    {
+        
+      for(j=i+1;j<5;j++)
+
+        {
+         
+            if(a[i]<a[j])
+
+            {
+   
+                temp=a[i];
+
+                a[i]=a[j];
+
+                a[j]=temp;
+
+            }
+ 
+       }
+
+    }
+
+    printf("Elements sorted in ascending order are\n");
+
+    for(i=0;i<5;i++)
+
+    {
+     
+     printf("%d\t",a[i]);
+
+    }
+
+}  
