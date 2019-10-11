a0412c507afcc7ff946585ed8879768012bd997d
piraneetha-it19 <54268012+piraneetha-it19@users.noreply.github.com>
2019-10-11 03:30:58 -0700

    second commit

---
+++ string character.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void main()
+
+{
+    
+    char s[10];
+
+    printf("enter a string");
+
+    scanf("%s",s);
+
+    for(int i=0;s[i]!='\0';i++)
+
+    {
+        
+ printf("%c\n",s[i]);
+
+    }
+
+}
